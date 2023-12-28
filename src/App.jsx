6ef8064abd26448ef10a8d5ff47fa8e56095ba72
Tonyliu2004 <ltsplice@gmail.com>
2023-12-27 20:48:22 -0500

    got AI to work

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,43 @@
 import { useState } from 'react'
 import './App.css'
 import React from 'react';
+import { GoogleGenerativeAI } from '@google/generative-ai';
+
 function App() {
+  const API_KEY = import.meta.env.VITE_APP_API_KEY;
   const [input, setInput] = useState("");
+  const [response, setResponse] = useState("");
   const handleSubmit = () => {
-    console.log(input);
+    if(input != "") {
+      console.log(input);
+      setInput("");
+      fetchData();
+    }
   }
+
+  async function fetchData(){
+    try {
+      const genAI = new GoogleGenerativeAI(API_KEY);
+      const model = genAI.getGenerativeModel({ model: "gemini-pro" });
+
+      const results = await model.generateContent(input);
+      console.log("RESULTS: ", results);
+      const response = results.response;
+      console.log("RESPONSE: ", response);
+      const text = response.text();
+      console.log("TEXT: ", text);
+      setResponse(text);
+    } catch (error){
+      setResponse("ERROR, try again");
+      console.log("ERROR: ", error);
+    }
+  }
+
   return (
     <div>
-      <input type="text" onChange={(event) => setInput(event.target.value)}/> <br/>
-      <button id = "searchButton" onClick = {handleSubmit}>Submit</button>
+      <input type="text" onChange={(event) => setInput(event.target.value)} value={input}/> <br/>
+      <button id = "searchButton" onClick = {handleSubmit}>Submit</button> <br/>
+      {response}
     </div>
   )
 }
