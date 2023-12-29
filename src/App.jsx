c7f11430172de200621533e12ee5369ced5501ea
JoseG777 <104945899+JoseG777@users.noreply.github.com>
2023-12-29 18:44:19 -0500

    organized imports

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,14 @@ import { useState } from 'react'
 import './App.css'
 import React from 'react';
 import { GoogleGenerativeAI } from '@google/generative-ai';
-import { db } from './firebaseConfig';
-import { collection, addDoc } from 'firebase/firestore';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import SignUp from './routes/SignUp';
 import SignIn from './routes/SignIn';
 import About from './routes/About';
 import Travel from './routes/Travel';
+import { db } from './firebaseConfig';
+import { collection, addDoc } from 'firebase/firestore';
 
 function App() {
   return (
@@ -20,7 +20,6 @@ function App() {
         <Route path="/travel" element={<Travel />} />
         <Route path="/signin" element={<SignIn />} />
         <Route path="/signup" element={<SignUp />} />
-        {/* Additional routes can be added here */}
       </Routes>
     </BrowserRouter>
   )
