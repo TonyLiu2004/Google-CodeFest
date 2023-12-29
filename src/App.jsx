362972d6dfdcd9501b165a53b2e7750a11fe7468
JoseG777 <104945899+JoseG777@users.noreply.github.com>
2023-12-29 18:41:15 -0500

    added navbar, moved travel generator

--- src/App.jsx
+++ src/App.jsx
@@ -4,176 +4,25 @@ import React from 'react';
 import { GoogleGenerativeAI } from '@google/generative-ai';
 import { db } from './firebaseConfig';
 import { collection, addDoc } from 'firebase/firestore';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import SignUp from './routes/SignUp';
+import SignIn from './routes/SignIn';
+import About from './routes/About';
+import Travel from './routes/Travel';
 
 function App() {
-  const API_KEY = import.meta.env.VITE_APP_API_KEY;
-
-  const [location, setLocation] = useState("");
-  const [budget, setBudget] = useState("");
-  const [activity, setActivity] = useState("");
-  const [response, setResponse] = useState("");
-
-  const [count, setCount] = useState(0); // will change display of questions
-  const [display, setDisplay] = useState(false);
-  //const [season, setSeason] = useState(""); // maybe change to month
-  //const [temp, setTemp] = useState("");
-  //const [weather, setWeather] = useState(""); // maybe remove
-  const [activityType, setActivityType] = useState([]); //they can pick multiple activities
-  const [activityString, setActivityString] = useState("");
-
-  const handleCheck = (event) => {
-    const value = event.target.value;
-    const isChecked = event.target.checked;
-
-    if (isChecked) {
-      setActivityType([...activityType, value]);
-    }
-    else {
-      setActivityType(activityType.filter((item) => item !== value));
-    }
-  }
-
-  const activitySubmit = (event) => {
-    event.preventDefault();
-    let activityString = "";
-
-    for (let i = 0; i < activityType.length; i++) {
-      activityString += activityType[i];
-      if (i != activityType.length - 1) {
-        activityString += ", ";
-      }
-    }
-
-    setCount(count + 1);
-    setActivityString(activityString);
-    setActivityType([]);
-    console.log(activityString);
-  }
-
-
-
-  let formatted = "";
-
-  const handleSubmit = () => {
-    if (location != "" && budget != "" && activity != "") {
-      setLocation("");
-      setBudget("")
-      setActivity("");
-      let selected_div = document.querySelector('.results');
-      selected_div.innerHTML = '';
-      setDisplay(true);
-      fetchData();
-    }
-  }
-
-  async function fetchData() {
-    try {
-      const genAI = new GoogleGenerativeAI(API_KEY);
-      const model = genAI.getGenerativeModel({ model: "gemini-pro" });
-
-      const results = await model.generateContent("I want to " + activity + " in " + location + ". My budget is " + budget);
-      console.log("RESULTS: ", results);
-      const response = results.response;
-      console.log("RESPONSE: ", response);
-      const text = response.text();
-      console.log("TEXT: ", text);
-      setResponse(text);
-    }
-    catch (error) {
-      setResponse("ERROR, try again");
-      console.log("ERROR: ", error);
-    }
-  }
-
-  if (response != "") {
-    let selected_div = document.querySelector('.results');
-    let list = response.split("\n");
-    for (let i = 0; i < list.length; i++) {
-      var each_item = document.createElement('p');
-      each_item.textContent = list[i];
-      selected_div.appendChild(each_item);
-    }
-    setResponse("");
-    list = {};
-  }
-
-
-
-  const makePDF = () => {
-
-    let pdfButton = document.getElementById("pdfButton");
-    let makepdf = document.getElementById("makepdf");
-    let mywindow = window.open("", "PRINT",
-      "height=400,width=600");
-
-    mywindow.document.write(makepdf.innerHTML);
-
-    mywindow.document.close();
-    mywindow.focus();
-
-    mywindow.print();
-    mywindow.close();
-
-    return true;
-  }
-
   return (
-    <div>
-      <h1>
-        Travel Planner
-      </h1>
-
-      {count === 0 && <form>
-
-        <label>
-
-          <input type="checkbox" value="Food and Drinks" onChange={handleCheck} /> Food and Drinks
-
-        </label>
-
-        <br></br>
-
-        <label>
-
-          <input type="checkbox" value="Sightseeing" onChange={handleCheck} /> Sightseeing
-
-        </label>
-
-        <br></br>
-
-        <label>
-
-          <input type="checkbox" value="Outdoor adventures" onChange={handleCheck} /> Outdoor adventures
-
-        </label>
-
-        <br></br>
-
-        <label>
-
-          <input type="checkbox" value="Shopping" onChange={handleCheck} /> Shopping
-
-        </label>
-
-        <br></br>
-        <button onClick={activitySubmit}> Submit </button>
-      </form>
-      }
-
-      <input type="text" placeholder="Location" onChange={(event) => setLocation(event.target.value)} value={location} /> <br />
-
-      <input type="text" placeholder="Budget" onChange={(event) => setBudget(event.target.value)} value={budget} /> <br />
-
-      <input type="text" placeholder="Activity" onChange={(event) => setActivity(event.target.value)} value={activity} /> <br />
-
-      <button id="searchButton" onClick={handleSubmit}>Submit</button> <br />
-
-      <br></br>
-      {display && <button id="pdfButton" onClick={makePDF}>Generate PDF</button>}
-
-      <div className='results' id='makepdf'></div>
-
-    </div>
+    <BrowserRouter>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<About />} />
+        <Route path="/travel" element={<Travel />} />
+        <Route path="/signin" element={<SignIn />} />
+        <Route path="/signup" element={<SignUp />} />
+        {/* Additional routes can be added here */}
+      </Routes>
+    </BrowserRouter>
   )
 }
 
