06c1ee51a16a6ab05d883ab37c21b212977cf067
Tonyliu2004 <ltsplice@gmail.com>
2023-12-29 22:05:36 -0500

    Added user signin/signout features

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,18 +1,32 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Link } from 'react-router-dom';
 
 function Navbar() {
+    const [userAuth, setUserAuth] = useState(
+        <>
+            <Link to="/signin" className="nav-link"> Sign In </Link>
+            <Link to="/signup" className="nav-link"> Sign Up </Link>
+        </>
+    );
+    useEffect(() => {
+        if(sessionStorage.getItem("accessToken") == null){
+            setUserAuth(
+                <>
+                    <Link to="/signin" className="nav-link"> Sign In </Link> 
+                    <Link to="/signup" className="nav-link"> Sign Up </Link>
+                </>
+            );
+        }else{
+            setUserAuth(
+                <Link className="nav-link" onClick={() => { sessionStorage.removeItem("accessToken"); location.reload(); }}> Logout </Link>
+            );
+        }
+    },[])
     return (
         <nav>
-
             <Link to="/" className="nav-link">About</Link>
-
             <div className="auth-links">
-
-                <Link to="/signin" className="nav-link"> Sign In </Link> /
-
-                <Link to="/signup" className="nav-link"> Sign Up </Link>
-
+                { userAuth }
             </div>
         </nav>
     );
--- src/routes/SignIn.jsx
+++ src/routes/SignIn.jsx
@@ -1,8 +1,48 @@
-import React from "react";
+import React, { useState } from "react";
+import { auth } from '../firebaseConfig';
+import { signInWithEmailAndPassword} from 'firebase/auth';
+import { useNavigate } from 'react-router-dom';
 
 function SignIn() {
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    let navigate = useNavigate();
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        signInWithEmailAndPassword(auth, email, password).then((user) => {
+            console.log(user); //signed in successfully
+            sessionStorage.setItem("accessToken", user.user.auth.lastNotifiedUid);
+            navigate('/');
+            location.reload();
+            alert("Signed In successfully!");
+        }).catch((error) => {
+            console.log("ERROR SIGNIN: ", error);
+            alert(error);
+        })
+    }
+
     return (
-        <div> This will be sign in page </div>
+        <div>
+        <h1> Sign In </h1>
+            <form>
+                <input
+                    type="email"
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                    placeholder="Email"
+                />
+                <br />
+                <input
+                    type="password"
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                    placeholder="Password"
+                />
+                <br />
+                <button type="submit" onClick={handleSubmit}> Submit </button>
+            </form>
+        </div>
     );
 }
 
