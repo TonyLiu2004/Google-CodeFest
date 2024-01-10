ce051503c193810b6bba48d11f651fb4560d66b5
NotEmily3203 <linemily62@gmail.com>
2024-01-10 14:37:43 -0500

    corrected another location bug

--- src/components/DIM.jsx
+++ src/components/DIM.jsx
@@ -268,10 +268,10 @@ function DIM({ dim }) {
             createdAt: new Date()
         });
     }
-
+    const [selectedCountry, setSelectedCountry] = useState('');
     if(dim=="Yes"){
     var country_list = ["None","Afghanistan","Albania","Algeria","Andorra","Angola","Anguilla","Antigua &amp; Barbuda","Argentina","Armenia","Aruba","Australia","Austria","Azerbaijan","Bahamas","Bahrain","Bangladesh","Barbados","Belarus","Belgium","Belize","Benin","Bermuda","Bhutan","Bolivia","Bosnia &amp; Herzegovina","Botswana","Brazil","British Virgin Islands","Brunei","Bulgaria","Burkina Faso","Burundi","Cambodia","Cameroon","Cape Verde","Cayman Islands","Chad","Chile","China","Colombia","Congo","Cook Islands","Costa Rica","Cote D Ivoire","Croatia","Cruise Ship","Cuba","Cyprus","Czech Republic","Denmark","Djibouti","Dominica","Dominican Republic","Ecuador","Egypt","El Salvador","Equatorial Guinea","Estonia","Ethiopia","Falkland Islands","Faroe Islands","Fiji","Finland","France","French Polynesia","French West Indies","Gabon","Gambia","Georgia","Germany","Ghana","Gibraltar","Greece","Greenland","Grenada","Guam","Guatemala","Guernsey","Guinea","Guinea Bissau","Guyana","Haiti","Honduras","Hong Kong","Hungary","Iceland","India","Indonesia","Iran","Iraq","Ireland","Isle of Man","Israel","Italy","Jamaica","Japan","Jersey","Jordan","Kazakhstan","Kenya","Kuwait","Kyrgyz Republic","Laos","Latvia","Lebanon","Lesotho","Liberia","Libya","Liechtenstein","Lithuania","Luxembourg","Macau","Macedonia","Madagascar","Malawi","Malaysia","Maldives","Mali","Malta","Mauritania","Mauritius","Mexico","Moldova","Monaco","Mongolia","Montenegro","Montserrat","Morocco","Mozambique","Namibia","Nepal","Netherlands","Netherlands Antilles","New Caledonia","New Zealand","Nicaragua","Niger","Nigeria","Norway","Oman","Pakistan","Palestine","Panama","Papua New Guinea","Paraguay","Peru","Philippines","Poland","Portugal","Puerto Rico","Qatar","Reunion","Romania","Russia","Rwanda","Saint Pierre &amp; Miquelon","Samoa","San Marino","Satellite","Saudi Arabia","Senegal","Serbia","Seychelles","Sierra Leone","Singapore","Slovakia","Slovenia","South Africa","South Korea","Spain","Sri Lanka","St Kitts &amp; Nevis","St Lucia","St Vincent","St. Lucia","Sudan","Suriname","Swaziland","Sweden","Switzerland","Syria","Taiwan","Tajikistan","Tanzania","Thailand","Timor L'Este","Togo","Tonga","Trinidad &amp; Tobago","Tunisia","Turkey","Turkmenistan","Turks &amp; Caicos","Uganda","Ukraine","United Arab Emirates","United Kingdom","Uruguay","Uzbekistan","Venezuela","Vietnam","Virgin Islands (US)","Yemen","Zambia","Zimbabwe"];
-    const [selectedCountry, setSelectedCountry] = useState('');
+    
     useEffect(() => {
         const countrySelect = document.getElementById("country");
         console.log(countrySelect);
@@ -281,15 +281,12 @@ function DIM({ dim }) {
           option.textContent = country;
           countrySelect.appendChild(option);
         }, []);
-        const handleChange = (event) => {
-            console.log(event.target.value);
-          setSelectedCountry(event.target.value);
-        };
-        countrySelect.addEventListener('change', handleChange);
-        return () => {
-          countrySelect.removeEventListener('change', handleChange);
-        };
-      }, []);
+        
+    
+    })}
+    const CountryHandleChange = (event) => {
+        console.log(event.target.value);
+        setSelectedCountry(event.target.value);
     }
 
     return (
@@ -385,7 +382,7 @@ function DIM({ dim }) {
                 <div>
                 <>
           <label htmlFor="country">Country Name: &nbsp;</label>
-          <select id="country"></select>
+          <select id="country" onChange={CountryHandleChange}></select>
           <br /><br />
         </>
                 <label htmlFor="style">Overall Style: &nbsp;</label>
