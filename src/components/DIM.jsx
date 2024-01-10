5f9fb2ca17afbc4b42779e931c6a666a2c1be155
NotEmily3203 <linemily62@gmail.com>
2024-01-10 03:39:11 -0500

    country selection and modding api conditions

--- src/components/DIM.jsx
+++ src/components/DIM.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import '../routes/Travel.css'
 import React from 'react';
 import { GoogleGenerativeAI } from '@google/generative-ai';
@@ -6,21 +6,21 @@ import { db, auth } from '../firebaseConfig';
 import { collection, addDoc, getDocs } from 'firebase/firestore';
 import { getStorage, ref, uploadBytes, getDownloadURL } from 'firebase/storage';
 import jsPDF from 'jspdf';
+import NDIM from './NDIM';
+
 
 function DIM({ dim }) {
     const API_KEY = import.meta.env.VITE_APP_API_KEY;
     const IMAGE_KEY = import.meta.env.IMAGE_KEY;
-
+    
     const [display, setDisplay] = useState(false);
     //const [pdf, setPDF] = useState(null);
     //const [pdfURL, setPDFURL] = useState("");
 
     //If they know where they want to go
-    const [location, setLocation] = useState("");
     const [budget, setBudget] = useState(0);
     const [activities, setActivities] = useState("");
     const [otherActivities, setOtherActivities] = useState("");
-
     const [duration, setDuration] = useState(1);
     const [group, setGroup] = useState(1);
     const [style, setStyle] = useState("");
@@ -40,7 +40,7 @@ function DIM({ dim }) {
         }
     }
 
-    const handleSubmit = () => {
+    /*const handleSubmit = () => {
         if (location != "" && budget != "" && activities != "") {
             setLocation("");
             setBudget("")
@@ -50,7 +50,7 @@ function DIM({ dim }) {
             setDisplay(true);
             fetchData("I want to " + activities + " in " + location + ". My budget is " + budget + ". Put it in a numbered list with a title and details. Include price rounded to the nearest whole number.");
         }
-    }
+    }*/
 
     function uncheckAllCheckboxes() {
         const checkboxes = document.querySelectorAll('input[type="checkbox"]');
@@ -59,6 +59,31 @@ function DIM({ dim }) {
             checkbox.checked = false;
         });
     }
+
+    
+    
+
+    /*let location ="";
+    
+    function generateLocation(prompt) {
+        const genAI = new GoogleGenerativeAI(API_KEY);
+        const model = genAI.getGenerativeModel({ model: "gemini-pro" });
+    
+        return model.generateContent(prompt)
+            .then(results => {
+                console.log("LRESULTS: ", results);
+                const response = results.response;
+                console.log(":RESPONSE: ", response);
+                const text = response.text();
+                console.log("LTEXT: \n", text);
+                location = text;
+            })
+            .catch(error => {
+                setLocation("ERROR, try again");
+                console.log("ERROR: ", error);
+            });
+    }*/
+
     const handleSubmitNDIM = () => {
         let activityString = "";
         if (activities != {} && activities != "") {
@@ -78,18 +103,34 @@ function DIM({ dim }) {
 
         //formatting temp, temp is the prompt
         let temp = "";
+        //let locationDetails = "";
         if (activityString == "") alert("Must choose an activity!");
-        else temp = "I want to do these activities: " + activityString + ". ";
+        else temp = "I want to do these activities: " + activityString + ". "; /*locationDetails= "I want to do these activities: " + activityString + ". "*/
         if (climate != "") temp += "I want to do them in a place with a " + climate + " climate. ";
-        if (style != "") temp += "The style I am looking for in this trip is a " + style + " style. ";
-        temp += "This trip will be " + duration + " days long, my group size is " + group + " and my budget is " + budget + " USD. Please put it in a numbered list with a title and details. Include price rounded to the nearest whole number.";
-        console.log(temp);
+        
+        
+        
+        if (style != "") temp += ". The style I am looking for in this trip is a " + style + " style. ";
+        temp += "This trip will be " + duration + " days long, my group size is " + group + " and my budget is " + budget + " USD.";
+        
         setDisplay(true);
 
+
+        if(dim=="No"){
+            temp+="Please mention one suitable country put the generated activities in a numbered list with a title and details. Include price rounded to the nearest whole number.";
+        }else{
+            if(selectedCountry =="") 
+            alert("Location cannot be empty, try again");
+            else
+            temp+="I would like to do the aforementioned activities in " + selectedCountry+". Please put the generated activities in a numbered list with a title and details. Include price rounded to the nearest whole number.";
+        }
+        console.log(temp);
         //resetting inputs
         uncheckAllCheckboxes();
+        if(dim=="No"){
         const climateSelect = document.getElementById('climate');
         climateSelect.value = '';
+        }
         const styleSelect = document.getElementById('style');
         styleSelect.value = '';
         setDuration(1);
@@ -98,7 +139,8 @@ function DIM({ dim }) {
         setOtherActivities("");
         setClimate("");
         setActivities("");
-
+        
+        
         fetchData(temp);
     }
     const aiOutputFilter = (input) => { //split the ai output into a list of strings
@@ -120,7 +162,6 @@ function DIM({ dim }) {
         try {
             const genAI = new GoogleGenerativeAI(API_KEY);
             const model = genAI.getGenerativeModel({ model: "gemini-pro" });
-
             const results = await model.generateContent(query);
             console.log("RESULTS: ", results);
             const response = results.response;
@@ -228,20 +269,167 @@ function DIM({ dim }) {
         });
     }
 
+    var country_list = ["None","Afghanistan","Albania","Algeria","Andorra","Angola","Anguilla","Antigua &amp; Barbuda","Argentina","Armenia","Aruba","Australia","Austria","Azerbaijan","Bahamas","Bahrain","Bangladesh","Barbados","Belarus","Belgium","Belize","Benin","Bermuda","Bhutan","Bolivia","Bosnia &amp; Herzegovina","Botswana","Brazil","British Virgin Islands","Brunei","Bulgaria","Burkina Faso","Burundi","Cambodia","Cameroon","Cape Verde","Cayman Islands","Chad","Chile","China","Colombia","Congo","Cook Islands","Costa Rica","Cote D Ivoire","Croatia","Cruise Ship","Cuba","Cyprus","Czech Republic","Denmark","Djibouti","Dominica","Dominican Republic","Ecuador","Egypt","El Salvador","Equatorial Guinea","Estonia","Ethiopia","Falkland Islands","Faroe Islands","Fiji","Finland","France","French Polynesia","French West Indies","Gabon","Gambia","Georgia","Germany","Ghana","Gibraltar","Greece","Greenland","Grenada","Guam","Guatemala","Guernsey","Guinea","Guinea Bissau","Guyana","Haiti","Honduras","Hong Kong","Hungary","Iceland","India","Indonesia","Iran","Iraq","Ireland","Isle of Man","Israel","Italy","Jamaica","Japan","Jersey","Jordan","Kazakhstan","Kenya","Kuwait","Kyrgyz Republic","Laos","Latvia","Lebanon","Lesotho","Liberia","Libya","Liechtenstein","Lithuania","Luxembourg","Macau","Macedonia","Madagascar","Malawi","Malaysia","Maldives","Mali","Malta","Mauritania","Mauritius","Mexico","Moldova","Monaco","Mongolia","Montenegro","Montserrat","Morocco","Mozambique","Namibia","Nepal","Netherlands","Netherlands Antilles","New Caledonia","New Zealand","Nicaragua","Niger","Nigeria","Norway","Oman","Pakistan","Palestine","Panama","Papua New Guinea","Paraguay","Peru","Philippines","Poland","Portugal","Puerto Rico","Qatar","Reunion","Romania","Russia","Rwanda","Saint Pierre &amp; Miquelon","Samoa","San Marino","Satellite","Saudi Arabia","Senegal","Serbia","Seychelles","Sierra Leone","Singapore","Slovakia","Slovenia","South Africa","South Korea","Spain","Sri Lanka","St Kitts &amp; Nevis","St Lucia","St Vincent","St. Lucia","Sudan","Suriname","Swaziland","Sweden","Switzerland","Syria","Taiwan","Tajikistan","Tanzania","Thailand","Timor L'Este","Togo","Tonga","Trinidad &amp; Tobago","Tunisia","Turkey","Turkmenistan","Turks &amp; Caicos","Uganda","Ukraine","United Arab Emirates","United Kingdom","Uruguay","Uzbekistan","Venezuela","Vietnam","Virgin Islands (US)","Yemen","Zambia","Zimbabwe"];
+    const [selectedCountry, setSelectedCountry] = useState('');
+    useEffect(() => {
+        const countrySelect = document.getElementById("country");
+        console.log(countrySelect);
+        country_list.forEach(country => {
+          const option = document.createElement("option");
+          option.value = country;
+          option.textContent = country;
+          countrySelect.appendChild(option);
+        }, []);
+        const handleChange = (event) => {
+            console.log(event.target.value);
+          setSelectedCountry(event.target.value);
+        };
+        countrySelect.addEventListener('change', handleChange);
+        return () => {
+          countrySelect.removeEventListener('change', handleChange);
+        };
+      }, []);
+    
+
     return (
         <div>
             {dim === "Yes" &&
+                <div id="theForm">
+                <form>
+                    <div id="activitiesTop">
+                        <h3 style={{ marginBottom: "0px" }}>Desired Activities:</h3>
+                        <div style={{ display: "flex" }}>
+                            <h3 style={{ marginBottom: "0px" }}> Other: &nbsp;</h3>
+                            <input type="text" id="otherActivities" value={otherActivities} placeholder="Other Activities" onChange={(event) => setOtherActivities(event.target.value)}></input>
+                        </div>
+                    </div>
+                    <div id="activitiesform">
+                        <div className="subActivities">
+                            <h4 style={{ marginBottom: "5px" }}>Outdoors</h4>
+                            <label>
+                                <input type="checkbox" value="Camping" onChange={handleActivity} /> Camping
+                            </label>
+                            <label>
+                                <input type="checkbox" value="Biking" onChange={handleActivity} /> Biking
+                            </label>
+                            <label>
+                                <input type="checkbox" value="Hiking" onChange={handleActivity} /> Hiking
+                            </label>
+                            <label>
+                                <input type="checkbox" value="Swimming" onChange={handleActivity} /> Swimming
+                            </label>
+                        </div>
+                        <div className="subActivities">
+                            <h4 style={{ marginBottom: "5px" }}>Cultural</h4>
+                            <label>
+                                <input type="checkbox" value="Museums and Art Galleries" onChange={handleActivity} /> Museums/Art
+                            </label>
+                            <label>
+                                <input type="checkbox" value="Historical tours" onChange={handleActivity} /> Historical tours
+                            </label>
+                            <label>
+                                <input type="checkbox" value="Local festivals" onChange={handleActivity} /> Local festivals
+                            </label>
+                            <label>
+                                <input type="checkbox" value="Performing arts" onChange={handleActivity} /> Performing arts
+                            </label>
+                        </div>
+                        <div className="subActivities">
+                            <h4 style={{ marginBottom: "5px" }}>City Exploration</h4>
+                            <label>
+                                <input type="checkbox" value="City Tours" onChange={handleActivity} /> City Tours
+                            </label>
+                            <label>
+                                <input type="checkbox" value="Shopping" onChange={handleActivity} /> Shopping
+                            </label>
+                            <label>
+                                <input type="checkbox" value="Food" onChange={handleActivity} /> Food
+                            </label>
+                            <label>
+                                <input type="checkbox" value="Urban Parks" onChange={handleActivity} /> Urban Parks
+                            </label>
+                        </div>
+                        <div className="subActivities">
+                            <h4 style={{ marginBottom: "5px" }}>Nature</h4>
+                            <label>
+                                <input type="checkbox" value="Safari Tours" onChange={handleActivity} /> Safari Tours
+                            </label>
+                            <label>
+                                <input type="checkbox" value="Bird Watching" onChange={handleActivity} /> Bird Watching
+                            </label>
+                            <label>
+                                <input type="checkbox" value="Nature Reserves" onChange={handleActivity} /> Nature Reserves
+                            </label>
+                            <label>
+                                <input type="checkbox" value="Sightseeing" onChange={handleActivity} /> Sightseeing
+                            </label>
+                        </div>
+                        <div className="subActivities">
+                            <h4 style={{ marginBottom: "5px" }}>Family</h4>
+                            <label>
+                                <input type="checkbox" value="Amusement Parks" onChange={handleActivity} /> Amusement Parks
+                            </label>
+                            <label>
+                                <input type="checkbox" value="Zoos and Aquariums" onChange={handleActivity} /> Zoos and Aquariums
+                            </label>
+                            <label>
+                                <input type="checkbox" value="Beach" onChange={handleActivity} /> Beach
+                            </label>
+                            <label>
+                                <input type="checkbox" value="Cruise" onChange={handleActivity} /> Cruise
+                            </label>
+                        </div>
+                    </div>
+                </form>
                 <div>
-                    <input type="text" placeholder="Location" onChange={(event) => setLocation(event.target.value)} value={location} /> <br />
+                <>
+          <label htmlFor="country">Country Name: &nbsp;</label>
+          <select id="country"></select>
+          <br /><br />
+        </>
+                <label htmlFor="style">Overall Style: &nbsp;</label>
+                    <select style={{ fontSize: "14px" }} name="style" id="style" form="styleform" onChange={() => setStyle(document.getElementById("style").value)}>
+                        <option value="">No Preference</option>
+                        <option value="Adventurous">Adventurous</option>
+                        <option value="Relaxed">Relaxed</option>
+                        <option value="Cultural">Cultural</option>
+                        <option value="Urban">Urban</option>
+                        <option value="Family">Family</option>
+                    </select>
+                    <br /><br />
+                    <label htmlFor="days">Number of Days: &nbsp;</label>
+                    <input
+                        type="number"
+                        id="days"
+                        value={duration}
+                        onChange={(event) => setDuration(Math.max(1, parseInt(event.target.value, 10)))}
+                        min="1"
+                    />
+                    <br /><br />
 
-                    <input type="text" placeholder="Activity" onChange={(event) => setActivities(event.target.value)} value={activities} /> <br />
+                    <label htmlFor="groupSize">Group Size: &nbsp;</label>
+                    <input
+                        type="number"
+                        id="groupSize"
+                        value={group}
+                        onChange={(event) => setGroup(Math.max(1, parseInt(event.target.value, 10)))}
+                        min="1"
+                    />
+                    <br /><br />
 
-                    <input type="text" placeholder="Budget" onChange={(event) => setBudget(event.target.value)} value={budget} /> <br /> <br />
-
-                    <button id="searchButton" onClick={handleSubmit}> Generate Itinerary </button> <br />
-
-                    <br></br>
+                    <label htmlFor="budget">Budget (USD): &nbsp;</label>
+                    <input
+                        type="number"
+                        id="budget"
+                        placeholder="Enter your budget in USD"
+                        value={budget}
+                        onChange={(event) => setBudget(Math.max(0, parseInt(event.target.value, 10)))}
+                    />
+                    <br /><br /><hr /><br />
+                    <button style={{ display: "block", margin: "0 auto" }} onClick={handleSubmitNDIM}> Generate Itinerary </button>
+                    <br />
                 </div>
+            </div>
             }
             {dim === "No" &&
                 <div id="theForm">
@@ -387,6 +575,9 @@ function DIM({ dim }) {
                     <br />
                 </div>
             }
+
+            
+
             <div className='results' id='makepdf' style={{ whiteSpace: 'pre-wrap' }}></div>
 
 
