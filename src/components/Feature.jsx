e2f0181fec70e90928aae7aa3de9b72b83570280
NotEmily3203 <linemily62@gmail.com>
2024-01-10 14:10:58 -0500

    home page decoration and about page images

--- src/components/Feature.jsx
+++ src/components/Feature.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import './feature.css'
 
-const Feature =({title,desc})=>{
+const Feature =({title,desc,imgUrl})=>{
     return(
         <div className='feature'>
+                <img src={imgUrl} alt="" />
                 <h3>{title}</h3>
                 <p>{desc}</p>
         </div>
