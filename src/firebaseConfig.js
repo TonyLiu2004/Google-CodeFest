a75f3b9484458e548132fc0a71977d01c5e8a4db
JoseG777 <104945899+JoseG777@users.noreply.github.com>
2024-01-09 22:40:57 -0500

    secure sensitive info

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -9,13 +9,13 @@ import { getAuth } from 'firebase/auth';
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyDFOuZzqcqjYmZefaaR2h1Lpcc0yohTh7U",
-  authDomain: "codefest-97b85.firebaseapp.com",
-  projectId: "codefest-97b85",
-  storageBucket: "codefest-97b85.appspot.com",
-  messagingSenderId: "913525027667",
-  appId: "1:913525027667:web:e7007d266da91e097c67a6",
-  measurementId: "G-9JDY9NV33E"
+  apiKey: import.meta.env.VITE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
