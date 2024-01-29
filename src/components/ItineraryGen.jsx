033faf42fc2a06cc0382f7c583032073fc1ea541
JoseG777 <104945899+JoseG777@users.noreply.github.com>
2024-01-29 00:19:54 -0500

    beginning to add images to saved pdf

--- src/components/ItineraryGen.jsx
+++ src/components/ItineraryGen.jsx
@@ -25,6 +25,7 @@ function ItineraryGenerator({ dim }) {
 
     //for pdf
     const [cardInformation, setCardInformation] = useState([]);
+
     const handleCardInformation = (info) => {
         setCardInformation((prevInformation) => {
             const updatedInformation = [...prevInformation];
@@ -114,6 +115,7 @@ function ItineraryGenerator({ dim }) {
 
         fetchData(temp);
     }
+
     const aiOutputFilter = (input) => { //split the ai output into a list of strings
         let ret = [];
         let n = 1;
@@ -252,15 +254,6 @@ function ItineraryGenerator({ dim }) {
         setResponse("");
     }
 
-    const savePdfUrlToFirestore = async (pdfUrl) => {
-        await addDoc(collection(db, "itineraries"), {
-            userId: auth.currentUser.uid,
-            pdfUrl,
-            displayName,
-            createdAt: new Date()
-        });
-    }
-
     const [selectedCountry, setSelectedCountry] = useState('');
     if (dim == "Yes") {
         var country_list = ["None", "Afghanistan", "Albania", "Algeria", "Andorra", "Angola", "Anguilla", "Antigua &amp; Barbuda", "Argentina", "Armenia", "Aruba", "Australia", "Austria", "Azerbaijan", "Bahamas", "Bahrain", "Bangladesh", "Barbados", "Belarus", "Belgium", "Belize", "Benin", "Bermuda", "Bhutan", "Bolivia", "Bosnia &amp; Herzegovina", "Botswana", "Brazil", "British Virgin Islands", "Brunei", "Bulgaria", "Burkina Faso", "Burundi", "Cambodia", "Cameroon", "Cape Verde", "Cayman Islands", "Chad", "Chile", "China", "Colombia", "Congo", "Cook Islands", "Costa Rica", "Cote D Ivoire", "Croatia", "Cruise Ship", "Cuba", "Cyprus", "Czech Republic", "Denmark", "Djibouti", "Dominica", "Dominican Republic", "Ecuador", "Egypt", "El Salvador", "Equatorial Guinea", "Estonia", "Ethiopia", "Falkland Islands", "Faroe Islands", "Fiji", "Finland", "France", "French Polynesia", "French West Indies", "Gabon", "Gambia", "Georgia", "Germany", "Ghana", "Gibraltar", "Greece", "Greenland", "Grenada", "Guam", "Guatemala", "Guernsey", "Guinea", "Guinea Bissau", "Guyana", "Haiti", "Honduras", "Hong Kong", "Hungary", "Iceland", "India", "Indonesia", "Iran", "Iraq", "Ireland", "Isle of Man", "Israel", "Italy", "Jamaica", "Japan", "Jersey", "Jordan", "Kazakhstan", "Kenya", "Kuwait", "Kyrgyz Republic", "Laos", "Latvia", "Lebanon", "Lesotho", "Liberia", "Libya", "Liechtenstein", "Lithuania", "Luxembourg", "Macau", "Macedonia", "Madagascar", "Malawi", "Malaysia", "Maldives", "Mali", "Malta", "Mauritania", "Mauritius", "Mexico", "Moldova", "Monaco", "Mongolia", "Montenegro", "Montserrat", "Morocco", "Mozambique", "Namibia", "Nepal", "Netherlands", "Netherlands Antilles", "New Caledonia", "New Zealand", "Nicaragua", "Niger", "Nigeria", "Norway", "Oman", "Pakistan", "Palestine", "Panama", "Papua New Guinea", "Paraguay", "Peru", "Philippines", "Poland", "Portugal", "Puerto Rico", "Qatar", "Reunion", "Romania", "Russia", "Rwanda", "Saint Pierre &amp; Miquelon", "Samoa", "San Marino", "Satellite", "Saudi Arabia", "Senegal", "Serbia", "Seychelles", "Sierra Leone", "Singapore", "Slovakia", "Slovenia", "South Africa", "South Korea", "Spain", "Sri Lanka", "St Kitts &amp; Nevis", "St Lucia", "St Vincent", "St. Lucia", "Sudan", "Suriname", "Swaziland", "Sweden", "Switzerland", "Syria", "Taiwan", "Tajikistan", "Tanzania", "Thailand", "Timor L'Este", "Togo", "Tonga", "Trinidad &amp; Tobago", "Tunisia", "Turkey", "Turkmenistan", "Turks &amp; Caicos", "Uganda", "Ukraine", "United Arab Emirates", "United Kingdom", "Uruguay", "Uzbekistan", "Venezuela", "Vietnam", "Virgin Islands (US)", "Yemen", "Zambia", "Zimbabwe"];
@@ -281,6 +274,20 @@ function ItineraryGenerator({ dim }) {
     const CountryHandleChange = (event) => {
         console.log(event.target.value);
         setSelectedCountry(event.target.value);
+    }
+
+    //Idea: save URL pdfs to an array then sequentially add them to the pdf file
+    const updatedSavedItineraries = async () => {
+
+    }
+
+    const savePdfUrlToFirestore = async (pdfUrl) => {
+        await addDoc(collection(db, "itineraries"), {
+            userId: auth.currentUser.uid,
+            pdfUrl,
+            displayName,
+            createdAt: new Date()
+        });
     }
 
     return (
