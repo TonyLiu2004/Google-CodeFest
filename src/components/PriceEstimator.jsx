fefb820052a74b5ad0395af4f068c21c85da7a5b
JoseG777 <104945899+JoseG777@users.noreply.github.com>
2024-01-29 22:14:34 -0500

    finished functionality

--- src/components/PriceEstimator.jsx
+++ src/components/PriceEstimator.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import { GoogleGenerativeAI } from '@google/generative-ai';
 import axios from 'axios';
 
 const API_URL = "https://test.api.amadeus.com/v2/shopping/flight-offers";
@@ -6,6 +7,7 @@ const TOKEN_URL = 'https://test.api.amadeus.com/v1/security/oauth2/token';
 
 const amadeusApiKey = import.meta.env.VITE_AMADEUS_API_KEY;
 const amadeusApiSecret = import.meta.env.VITE_AMADEUS_API_SECRET;
+const geminiApiKey = import.meta.env.VITE_APP_API_KEY;
 
 const FlightSearchComponent = () => {
     const [origin, setOrigin] = useState('');
@@ -16,8 +18,13 @@ const FlightSearchComponent = () => {
     const [infants, setInfants] = useState(0);
     const [departureDate, setDepartureDate] = useState('');
     const [returnDate, setReturnDate] = useState('');
-    //const [flightClass, setFlightClass] = useState('ECONOMY');
+    const [flightClass, setFlightClass] = useState('ECONOMY');
     const [flights, setFlights] = useState([]);
+    const [display, setDisplay] = useState(true);
+    const [isLoading, setIsLoading] = useState(false);
+    const [noResults, setNoResults] = useState(false);
+    const [formattedFlightData, setFormattedFlightData] = useState('');
+    const [response, setResponse] = useState('');
 
     const fetchAccessToken = async () => {
         const params = new URLSearchParams();
@@ -39,15 +46,29 @@ const FlightSearchComponent = () => {
     };
 
     const getCheapestFlights = async (token) => {
+        setIsLoading(true);
+        setNoResults(false);
         try {
-            const response = await axios.get(`${API_URL}?originLocationCode=${origin}&destinationLocationCode=${destination}&departureDate=${departureDate}&returnDate=${returnDate}&adults=${adults}&children=${children}&infants=${infants}&nonStop=${nonStop}&currencyCode=USD&max=2`, {
+            const response = await axios.get(`${API_URL}?originLocationCode=${origin}&destinationLocationCode=${destination}&departureDate=${departureDate}&returnDate=${returnDate}&adults=${adults}&children=${children}&infants=${infants}&travelClass=${flightClass}&nonStop=${nonStop}&currencyCode=USD&max=1`, {
                 headers: {
                     'Authorization': `Bearer ${token}`
                 }
             });
-            setFlights(response.data.data);
+            if (response.data.data.length === 0) {
+                setNoResults(true);
+            } else {
+                setFlights(response.data.data);
+                const formattedData = formatFlightData(response.data.data);
+                setFormattedFlightData(formattedData);
+                console.log(formattedFlightData);
+                const aiInput = `${formattedFlightData} Convert this into english sentences. It is from an API that returns the details of a flight. Make it straight forward please. No extra just make the data into easy to read english sentences please`
+                await fetchData(aiInput);
+            }
         } catch (error) {
             console.error("Error fetching flights", error);
+        } finally {
+            setIsLoading(false);
+            setDisplay(false);
         }
     };
 
@@ -58,70 +79,102 @@ const FlightSearchComponent = () => {
             await getCheapestFlights(token);
         } catch (error) {
             console.error('Error in handleSearch:', error);
+            setIsLoading(false);
         }
     };
 
+    const formatFlightData = (flights) => {
+        return flights.map((flight, flightIndex) => {
+            return flight.itineraries.map((itinerary, itineraryIndex) => {
+                return itinerary.segments.map((segment, segmentIndex) => {
+                    const departure = segment.departure;
+                    const arrival = segment.arrival;
+                    const duration = segment.duration.replace('PT', '').replace('H', 'h ').replace('M', 'm');
+                    const stops = segment.numberOfStops === 0 ? 'Direct' : `${segment.numberOfStops} stops`;
+                    return `Segment ${segmentIndex + 1}: ${departure.iataCode} to ${arrival.iataCode}\n` +
+                        `Carrier: ${segment.carrierCode}, Flight Number: ${segment.number}\n` +
+                        `Departure: ${departure.at}, Arrival: ${arrival.at}\n` +
+                        `Duration: ${duration}, Stops: ${stops}\n`;
+                }).join('\n');
+            }).join('\n');
+        }).join('\n') + `\nPrice: ${flights[0].price.total}`;
+    };
+
+    async function fetchData(query) {
+        try {
+            const genAI = new GoogleGenerativeAI(geminiApiKey);
+            const model = genAI.getGenerativeModel({ model: "gemini-pro" });
+            const results = await model.generateContent(query);
+
+            const response = results.response;
+
+            const text = response.text();
+
+            setResponse(text);
+        }
+        catch (error) {
+            setResponse("ERROR, try again");
+            console.log("ERROR: ", error);
+        }
+    }
     return (
         <div>
-            <form onSubmit={handleSearch}>
-                <input type="text" value={origin} onChange={(e) => setOrigin(e.target.value)} placeholder="Origin" />
-
-                <br />
-
-                <input type="text" value={destination} onChange={(e) => setDestination(e.target.value)} placeholder="Destination" />
-
-                <br />
-
-                <input type="number" value={adults} onChange={(e) => setAdults(e.target.value)} />
-
-                <br />
-
-                <input type="number" value={children} onChange={(e) => setChildren(e.target.value)} />
-
-                <br />
-
-                <input type="number" value={infants} onChange={(e) => setInfants(e.target.value)} />
-
-                <br />
-
-                <label>
-                    Non-stop flight?
-                    <br />
-                    <input
-                        type="checkbox"
-                        checked={nonStop}
-                        onChange={(e) => setNonStop(e.target.checked)}
-                    />
-                </label>
-
-                <br />
-
-                <input type="date" value={departureDate} onChange={(e) => setDepartureDate(e.target.value)} />
-
-                <br />
-
-                <input type="date" value={returnDate} onChange={(e) => setReturnDate(e.target.value)} />
-
-                <br />
-
-                <button type="submit">Search Flights</button>
-            </form>
-
-            {flights.length > 0 && (
-                <div>
-                    <h2>Flight Results</h2>
-                    {flights.map((flight, index) => (
-                        <div key={index}>
-                            <p>{`From: ${flight.itineraries[0].segments[0].departure.iataCode}, To: ${flight.itineraries[0].segments[0].arrival.iataCode}`}</p>
-                            <p>{`Carrier: ${flight.itineraries[0].segments[0].carrierCode}`}</p>
-                            <p>{`Departure Time: ${flight.itineraries[0].segments[0].departure.at}`}</p>
-                            <p>{`Arrival Time: ${flight.itineraries[0].segments[0].arrival.at}`}</p>
-                            <p>{`Price: ${flight.price.total}`}</p>
+            {isLoading ? (
+                <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>Loading...</div>
+            ) : (
+                <>
+                    {display && (
+                        <div>
+                            <form onSubmit={handleSearch}>
+                                <input type="text" value={origin} onChange={(e) => setOrigin(e.target.value)} placeholder="Origin" />
+                                <br />
+                                <input type="text" value={destination} onChange={(e) => setDestination(e.target.value)} placeholder="Destination" />
+                                <br />
+                                <label>
+                                    Adults:
+                                    <input type="number" value={adults} onChange={(e) => setAdults(e.target.value)} placeholder="Adults" />
+                                </label>
+                                <br />
+                                <label>
+                                    Children:
+                                    <input type="number" value={children} onChange={(e) => setChildren(e.target.value)} placeholder="Children" />
+                                </label>
+                                <br />
+                                <label>
+                                    Infants:
+                                    <input type="number" value={infants} onChange={(e) => setInfants(e.target.value)} placeholder="Infants" />
+                                </label>
+                                <br />
+                                <label>
+                                    Non-stop flight?
+                                    <input
+                                        type="checkbox"
+                                        checked={nonStop}
+                                        onChange={(e) => setNonStop(e.target.checked)}
+                                    />
+                                </label>
+                                <br />
+                                <input type="date" value={departureDate} onChange={(e) => setDepartureDate(e.target.value)} />
+                                <br />
+                                <input type="date" value={returnDate} onChange={(e) => setReturnDate(e.target.value)} />
+                                <br />
+                                <button type="submit">Search Flights</button>
+                            </form>
                         </div>
-                    ))}
-                </div>
-            )}
+                    )}
 
+                    {noResults && (
+                        <div>No flights found for the specified criteria. Please try again with different parameters.</div>
+                    )}
+
+                    {flights.length > 0 && (
+                        <div>
+                            <h2>Flight Results</h2>
+                            <p>{response}</p>
+                        </div>
+                    )}
+                </>
+            )}
         </div>
     );
 };
