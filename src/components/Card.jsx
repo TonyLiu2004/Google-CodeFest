6eee9674cff1c7be9cdac485d13a6713d463e627
Tonyliu2004 <ltsplice@gmail.com>
2024-02-06 12:23:23 -0500

    test

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -10,29 +10,29 @@ const Card = ({ input, index }) => {
         import.meta.env.VITE_IMAGE_API_KEY
     );
 
-    useEffect(() => {
-        if (input === "") return;
+    // useEffect(() => {
+    //     if (input === "") return;
 
-        const fetchData = async () => {
-            try {
-                const response = await fetch(`https://api.pexels.com/v1/search?per_page=1&query=${input}`, {
-                    headers: {
-                        Authorization: import.meta.env.VITE_IMAGE_API_KEY,
-                    }
-                });
+    //     const fetchData = async () => {
+    //         try {
+    //             const response = await fetch(`https://api.pexels.com/v1/search?per_page=1&query=${input}`, {
+    //                 headers: {
+    //                     Authorization: import.meta.env.VITE_IMAGE_API_KEY,
+    //                 }
+    //             });
 
-                if (!response.ok) {
-                    alert('Network response was not ok');
-                }
-                const data = await response.json();
-                setImg(data.photos[0].src.original);
+    //             if (!response.ok) {
+    //                 alert('Network response was not ok');
+    //             }
+    //             const data = await response.json();
+    //             setImg(data.photos[0].src.original);
 
-            } catch (error) {
-                alert('Error fetching data:', error);
-            }
-        };
-        if (input != "") fetchData();
-    }, [input]);
+    //         } catch (error) {
+    //             alert('Error fetching data:', error);
+    //         }
+    //     };
+    //     if (input != "") fetchData();
+    // }, [input]);
     //console.log(img);
 
     return (
