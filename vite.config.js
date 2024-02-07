e2847e5ca279190d34508dfc9f76de3106e1412f
Tonyliu2004 <ltsplice@gmail.com>
2024-02-07 13:40:27 -0500

    changed vite config

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,10 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// vite.config.js
+import React from 'react';
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: 'https://tonyliu2004.github.io/Google-CodeFest/',
   plugins: [react()],
-})
+});
