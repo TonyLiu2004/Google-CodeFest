39fdb04740af48ab439acebbf924d584162fb0bf
Tonyliu2004 <ltsplice@gmail.com>
2024-02-07 13:46:24 -0500

    undid some stuff, still dont work

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,8 @@
 // vite.config.js
-import React from 'react';
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: 'https://tonyliu2004.github.io/Google-CodeFest/',
   plugins: [react()],
 });
